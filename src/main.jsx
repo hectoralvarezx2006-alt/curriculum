0978c2b741d42bedc8118ea37baedd4341abe3fa
Héctor Alvarez Pérez <hector_alvarez@ceroca.cat>
2026-05-07 19:56:03 +0200

    fix top-level await

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-// Simple client-side router sin dependencias extra
-const path = window.location.pathname
-
-let Component
-if (path === '/chat') {
-  const { default: ChatPage } = await import('./pages/ChatPage.jsx')
-  Component = ChatPage
-} else {
-  const { default: App } = await import('./App.jsx')
-  Component = App
-}
-
 window.scrollTo(0, 0)
 history.scrollRestoration = 'manual'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Component />
-  </React.StrictMode>
-)
+const path = window.location.pathname
+
+async function init() {
+  let Component
+  if (path === '/chat') {
+    const mod = await import('./pages/ChatPage.jsx')
+    Component = mod.default
+  } else {
+    const mod = await import('./App.jsx')
+    Component = mod.default
+  }
+
+  ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <Component />
+    </React.StrictMode>
+  )
+}
+
+init()
